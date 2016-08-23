fc8e020ae6c32c1805f61e5536e6e2b65115c5f8
buahho <buahho@gmail.com>
2016-08-23 03:12:08 +0200

    Update metadata.rb
    Changes recipe name

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ description      "Installs a script that resolve geoip location of DNS node attr
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 
-recipe "geoiplookup-test", "Installs a script that resolve geoip location of DNS node attributes."
+recipe "geoip-lookup", "Installs a script that resolve geoip location of DNS node attributes."
 
 supports "ubuntu"
